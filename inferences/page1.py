f45c07534d92c18146dc6f316b12ba8a01a18844
sick2024471 <126737248+sick2024471@users.noreply.github.com>
2023-05-18 12:12:18 +0900

    Update page1.py

--- inferences/page1.py
+++ inferences/page1.py
@@ -1,1 +1,10 @@
+import streamlit as st
 
+st.title('추론하는 프로그램')
+st.header('추론 방식을 선택하세요')
+
+if st.button("귀납법"):
+  st.write("귀납법 추론을 선택하셨습니다.")
+
+if st.button("연역법"):
+  st.write("연역법 추론을 선택하셨습니다.")
