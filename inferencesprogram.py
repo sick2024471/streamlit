d4857845780b2b8215351e7c125e8ca91e18e600
sick2024471 <126737248+sick2024471@users.noreply.github.com>
2023-05-18 10:43:55 +0900

    Update inferencesprogram.py

--- inferencesprogram.py
+++ inferencesprogram.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
-st.title('this is title')
-st.header('this is headder')
-st.subheader('this is subheader')
+col1,col2 = st.colums([2,3])
+
+col1.title('추론하는 프로그램')
+col1.header('추론 방식을 선택하세요')
+col2.title('어떻게 추론 하시겠습니까?')
