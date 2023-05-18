4c821b81d3088d969ae7583540e25974507e85b2
sick2024471 <126737248+sick2024471@users.noreply.github.com>
2023-05-18 11:05:14 +0900

    Update inferencesprogram.py

--- inferencesprogram.py
+++ inferencesprogram.py
@@ -3,10 +3,8 @@ import streamlit as st
 st.title('추론하는 프로그램')
 st.header('추론 방식을 선택하세요')
 
-btn_clicked1 = st.button("귀납법")
-btn_clicked2 = st.button("귀납법")
-if btn_clicked1:
+if st.button("귀납법"):
   st.write("귀납법 추론을 선택하셨습니다.")
 
-if btn_clicked2:
+if st.button("연역법"):
   st.write("연역법 추론을 선택하셨습니다.")
