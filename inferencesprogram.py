e4f95ec7d0b83c30b627854f73c51fe02a018797
sick2024471 <126737248+sick2024471@users.noreply.github.com>
2023-05-19 17:33:19 +0900

    Update inferencesprogram.py

--- inferencesprogram.py
+++ inferencesprogram.py
@@ -1,4 +1,11 @@
+import openai
 import streamlit as st
+
+openai.api_key = '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'
+
+messages = []
+content = ""
+chat_response = ""
 
 class MultiApp:
     def __init__(self):
@@ -26,11 +33,24 @@ def g():
     st.title("귀납법 프로그램 실행합니다.")
     Ginfer1 = st.text_input("개별적 사실", value = "여러문장 입력시 ,로 구분해서 쓰시오")
     Ginfer2 = st.text_input("소전체")
+    if st.button("입력"):
+        content = "귀납법으로 추론, 개별적 사실은 {Ginfer1}, 소전제는 {Ginfer2}, 결론만 출력"
+
 def y():
     st.title("연역법 프로그램 실행합니다.")
     Yinfer1 = st.text_input("대전체")
     Yinfer2 = st.text_input("소전체")
+    if st.button("입력"):
+        content = "연역법으로 추론, 대전제는 {Yinfer1} 소전제는 {Yinfer2}, 결론만 출력"
 infer = MultiApp()
 infer.add_app("귀납법 입력", g)
 infer.add_app("연역법 입력", y)
 infer.run()
+
+completion = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages = [{"role":"user", "content":content}]
+)
+chat_response = completion.choices[0].message.content
+st.write(chat_response)
+messages = [{"role": "assistant", "content": chat_response}]
