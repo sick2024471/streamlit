8492edb1c0ae739ef08bbba454bb6926bcc750f4
sick2024471 <126737248+sick2024471@users.noreply.github.com>
2023-05-19 16:44:56 +0900

    Update inferencesprogram.py

--- inferencesprogram.py
+++ inferencesprogram.py
@@ -13,9 +13,8 @@ class MultiApp:
 
     def run(self):
 
-        st.sidebar.title("Python Stat Tools v3.2")
-        st.sidebar.subheader("by Ken Harmon")
-        st.session_state.gs_URL = st.sidebar.text_input("Public Google Sheet URL:","https://docs.google.com/spreadsheets/d/1Fx7f6rM5Ce331F9ipsEMn-xRjUKYiR3R_v9IDBusUUY/edit#gid=0") 
+        st.sidebar.title("추론하는 프로그램")
+        st.sidebar.subheader("어떤법으로 추론하시겠습니까?")
                
         app = st.sidebar.radio(
             '',
@@ -25,10 +24,13 @@ class MultiApp:
         app['function']()
 def g():
     st.title("귀납법 프로그램 실행합니다.")
-
+    Ginfer1 = st.text_input("대전체")
+    Ginfer2 = st.text_input
 def y():
     st.title("연역법 프로그램 실행합니다.")
+    Yinfer1 = st.text_input("대전체")
+    Yinfer2 = st.text_input("소전체")
 infer = MultiApp()
 infer.add_app("귀납법 입력", g)
-infer.add_app("귀납법 입력", y)
+infer.add_app("연역법 입력", y)
 infer.run()
