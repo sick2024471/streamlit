d014729b913275b265995468a67c2b8d65644164
sick2024471 <126737248+sick2024471@users.noreply.github.com>
2023-05-20 15:56:42 +0900

    Update inferencesprogram.py

--- inferencesprogram.py
+++ inferencesprogram.py
@@ -1,11 +1,12 @@
 import openai
 import streamlit as st
 
-openai.api_key = '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'
-
 messages = []
 content = ""
 chat_response = ""
+
+
+openai.api_key = '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'
 
 class MultiApp:
     def __init__(self):
@@ -29,28 +30,46 @@ class MultiApp:
             format_func=lambda app: app['title'])
     
         app['function']()
+
 def g():
+    global content
     st.title("귀납법 프로그램 실행합니다.")
-    Ginfera = st.text_input("개별적 사실", value = "여러문장 입력시 ,로 구분해서 쓰시오")
-    Ginferb = st.text_input("소전제")
+    Ginfer1 = st.text_input("개별적 사실", value = "여러 문장 입력시 ,로 구분해서 쓰시오")
+    Ginfer2 = st.text_input("소전제")
     if st.button("입력"):
-        content = "귀납법으로 추론, 개별적 사실은 {Ginfer1}, 소전제는 {Ginfer2}, 결론만 출력"
+        content = f"귀납법으로 추론, 개별적 사실은 {Ginfer1}, 소전제는 {Ginfer2}, 결론만 출력"
+        completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": content}
+            ]
+        )
+
+    # 응답 결과 표시
+        chat_response = completion.choices[0].message.content
+        st.text(chat_response)
 
 def y():
+    global content
     st.title("연역법 프로그램 실행합니다.")
-    Yinfera = st.text_input("대전제")
-    Yinferb = st.text_input("소전제")
+    Yinfer1 = st.text_input("대전체")
+    Yinfer2 = st.text_input("소전체")
     if st.button("입력"):
-        content = "연역법으로 추론, 대전제는 {Yinfer1} 소전제는 {Yinfer2}, 결론만 출력"
+        content = f"연역법으로 추론, 대전제는 {Yinfer1} 소전제는 {Yinfer2}, 결론만 출력"
+        completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": content}
+            ]
+        )
+
+    # 응답 결과 표시
+        chat_response = completion.choices[0].message.content
+        st.text(chat_response)
+        
 infer = MultiApp()
 infer.add_app("귀납법 입력", g)
 infer.add_app("연역법 입력", y)
 infer.run()
-
-completion = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages = [{"role":"user", "content":content}]
-)
-chat_response = completion.choices[0].message.content
-st.write(chat_response)
-messages = [{"role": "assistant", "content": chat_response}]
