4e1703a36e0a5310669f755794589dfa3ca3820c
sick2024471 <126737248+sick2024471@users.noreply.github.com>
2023-05-25 11:26:14 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ import streamlit as st
 
 st.title("귀납법 프로그램 실행합니다.")
 number = st.number_input("개별적 사실 몇개를 입력하시겠습니까?",1,5)
+i = 0
 if st.button("결정"):
   Ginfer1 = []
-  for i in range(number):
+  while i < number:
     Ginfer1.append(st.text_input("개별적 사실"))
-    
+    i++
   Ginfer2 = st.text_input("소전제")
 
