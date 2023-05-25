a26380705bcc25669aa98527587a6534f2446c1e
sick2024471 <126737248+sick2024471@users.noreply.github.com>
2023-05-25 11:47:25 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ import streamlit as st
 
 st.title("귀납법 프로그램 실행합니다.")
 Ginfer1 = []
-for i in range(len(Ginfer1)):
-  Ginfer1[i] = st.text_input("개별적 사실을 입력 받으세요")
+for i in range(5):
   st.write(Ginfer[i])
+  fact = st.text_input("개별적 사실을 입력 받으세요")
+  Ginfer1.append(fact)
 Ginfer2 = st.text_input("소전제")
 
