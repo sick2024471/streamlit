b1a886e8ef03c9b52e0f1e7c044e3af972663166
sick2024471 <126737248+sick2024471@users.noreply.github.com>
2023-05-25 12:22:36 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title("귀납법 프로그램 실행합니다.")
 Ginfer1 = [0, 0, 0]
 for i in range(len(Ginfer1)):
-  fact = st.text_box("개별적 사실을 입력 받으세요")
+  fact = st.text_input("개별적 사실을 입력 받으세요")
   Ginfer1.append(fact)
 Ginfer2 = st.text_input("소전제")
 
