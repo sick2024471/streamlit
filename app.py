ca91570fc5401edb2fb5f3985f9f7bbdcf22c864
sick2024471 <126737248+sick2024471@users.noreply.github.com>
2023-05-25 11:41:17 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 import streamlit as st
 
 st.title("귀납법 프로그램 실행합니다.")
-number = st.number_input("개별적 사실 몇개를 입력하시겠습니까?",1,5)
-i = 0
-if st.button("결정"):
-  Ginfer1 = []
-  for i in range(number):
-    Ginfer1[i] = st.text_input("개별적 사실을 입력 받으세요")
-    st.write(Ginfer[i])
-  Ginfer2 = st.text_input("소전제")
+Ginfer1 = []
+for i in range(len(Ginfer1)):
+  Ginfer1[i] = st.text_input("개별적 사실을 입력 받으세요")
+  st.write(Ginfer[i])
+Ginfer2 = st.text_input("소전제")
 
