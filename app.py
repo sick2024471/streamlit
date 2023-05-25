451102d418748e7ee5a70dddcf3479aba1d4fbed
sick2024471 <126737248+sick2024471@users.noreply.github.com>
2023-05-25 11:31:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ number = st.number_input("개별적 사실 몇개를 입력하시겠습니까?",
 i = 0
 if st.button("결정"):
   Ginfer1 = []
-  while i < number:
-    Ginfer1.append(st.text_input("개별적 사실"))
-    i += 1
+  for i in range(number):
+    Ginfer1[i] = st.text_input("개별적 사실을 입력 받으세요")
+    st.write(Ginfer[i])
   Ginfer2 = st.text_input("소전제")
 
