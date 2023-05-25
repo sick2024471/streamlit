57e095d46a99d059a4c5ee8ef59c5de7d37c3a39
sick2024471 <126737248+sick2024471@users.noreply.github.com>
2023-05-25 10:28:23 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
-st.sidebar.title("This is written inside the side-bar")
-st.sidebar.button("Click")
-st.sidebar.radio("Pick your gender", ["Male", "Female"])
+st.title("귀납법 프로그램 실행합니다.")
+number = st.number_input("개별적 사실 몇개를 입력하시겠습니까?")
+Ginfer1 = st.text_input("개별적 사실", value = "여러 문장 입력시 ,로 구분해서 쓰시오")
+Ginfer2 = st.text_input("소전제")
+
