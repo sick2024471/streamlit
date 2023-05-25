1fd86a7662665e54b672f18c73977b36601cccb2
sick2024471 <126737248+sick2024471@users.noreply.github.com>
2023-05-25 11:09:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ st.title("귀납법 프로그램 실행합니다.")
 number = st.number_input("개별적 사실 몇개를 입력하시겠습니까?",1,5)
 if st.button("결정"):
   Ginfer1 = []
-  for i in range(number)):
+  for i in range(number):
     Ginfer1.append(st.text_input("개별적 사실"))
     
   Ginfer2 = st.text_input("소전제")
