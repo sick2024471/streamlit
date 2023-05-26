c9e64e9dc8e33c6b2cbe9da35c65aa9bcef0dc41
sick2024471 <126737248+sick2024471@users.noreply.github.com>
2023-05-26 17:54:10 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ number = st.number_input("개별적 사실 몇개 입력하시겠습니까?",1,5
 for i in range(number):
     fact = st.text_input("개별적 사실을 입력하세요", key = i)
     if st.button("∧(and)", key="and"):
-        st.text_input(label="", value="∧", key="and_in")
+        st.text_input(label="", value= fact + "∧", key="and_in")
 
     if st.button("∨(or)", key="or"):
         st.text_input(label="", value=str("∨"), key="or_in")
